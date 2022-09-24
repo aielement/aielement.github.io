0df269ec42176758b7d1bd010092aa7cece636f9
石上朗 <a.i.0326.element@gmail.com>
2022-09-24 19:22:23 +0900

    Noteの記事を取得し、Blogページに表示するようにした

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,30 @@
 import Layout from "../../components/Layout";
 import generalStyle from "../../styles/GeneralContent.module.css"
+import mainStyles from '../../styles/Home.module.css'
+import { GetMyNotePost } from "../../lib/getMyNotePost"
+import ActionAreaCard from "../../components/ActionAreaCard";
 
-export default function Blog(){
+
+export async function getStaticProps() {
+    const allNotePostData = await GetMyNotePost()
+
+    return {
+        props: {
+            allNotePostData,
+        },
+    };
+
+}
+
+export default function Blog( {allNotePostData} ){
     return (
         <Layout>
             <main className={generalStyle}>
-                <h1>工事中</h1>
+                {
+                    allNotePostData.map( ({ name, noteUrl, publishAt }) => (
+                        <ActionAreaCard route={noteUrl} title={name} content={publishAt}/>
+                    ))
+                }
             </main>
         </Layout>
     )
