5404ab3417c574e525ca240d84530e7cce760e70
石上朗 <a.i.0326.element@gmail.com>
2022-09-25 01:09:21 +0900

    zennの記事を取得し、Blogページに表示するようにした

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,27 +3,51 @@ import generalStyle from "../../styles/GeneralContent.module.css"
 import mainStyles from '../../styles/Home.module.css'
 import { GetMyNotePost } from "../../lib/getMyNotePost"
 import ActionAreaCard from "../../components/ActionAreaCard";
+import {GetMyZennFeed} from "../../lib/getMyZennFeed";
 
 
 export async function getStaticProps() {
     const allNotePostData = await GetMyNotePost()
+    const allZennFeedData = await GetMyZennFeed()
+
+    console.log(allNotePostData)
+    console.log(allZennFeedData)
+
+    const concatPostData = allNotePostData.concat(allZennFeedData)
+
+    const allPostData = concatPostData.sort(
+        function (a, b) {
+            const aDate = new Date(a.publishAt).getTime()
+            const bDate = new Date(b.publishAt).getTime()
+            return bDate - aDate
+        }
+    );
 
     return {
         props: {
-            allNotePostData,
+            allPostData
         },
     };
 
 }
 
-export default function Blog( {allNotePostData} ){
+export default function Blog( {allPostData} ){
     return (
         <Layout>
             <main className={generalStyle}>
                 {
-                    allNotePostData.map( ({ name, noteUrl, publishAt }) => (
-                        <ActionAreaCard key={name} route={noteUrl} title={name} content={publishAt} image="/notelogo.svg"/>
-                    ))
+                    allPostData.map( ({ title, url, publishAt, site }) => {
+                        if (site === "note") {
+                            return (
+                                <ActionAreaCard key={title} route={url} title={title} content={publishAt} image="/notelogo.svg"/>
+                            )
+                        }
+                        else if (site === "zenn") {
+                            return (
+                                <ActionAreaCard key={title} route={url} title={title} content={publishAt} image="/zennlogo.svg"/>
+                            )
+                        }
+                    })
                 }
             </main>
         </Layout>
